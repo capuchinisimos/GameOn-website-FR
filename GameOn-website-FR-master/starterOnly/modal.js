defee6e8967a290b4ae83f47decc1a4312e64718
oceane <oceaneverdi@protonmail.com>
2023-05-25 14:25:59 +0200

    declaration des variables

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -11,6 +11,15 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const cerrar = document.querySelector(".close");
+const nombre = document.querySelector("#first");
+const apellido = document.querySelector("#last");
+const email = document.querySelector("#email");
+const nacimiento = document.querySelector("#birthdate");
+const quantidad = document.querySelector("#quantity");
+const localizar = document.querySelector("#city");
+const terminos = document.querySelector("#checkbox1");
+const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
