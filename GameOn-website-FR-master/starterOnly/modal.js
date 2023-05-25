75ae383782663a89e9df30cce75ee530befa36d5
oceane <oceaneverdi@protonmail.com>
2023-05-25 16:09:28 +0200

    new commit

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -19,7 +19,16 @@ const nacimiento = document.querySelector("#birthdate");
 const quantidad = document.querySelector("#quantity");
 const localizar = document.querySelector("#city");
 const terminos = document.querySelector("#checkbox1");
-const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+const regexMain = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+const form = document.querySelector("#formulario");
+// erreurs
+const errorNombre = document.querySelector("#errorNombre");
+const errorApellido = document.querySelector("#errorApellido");
+const errorEmail = document.querySelector("#errorEmail");
+const errorNacimiento = document.querySelector("#errorNacimiento");
+const errorQuantidad = document.querySelector("#errorQuantidad");
+const errorLocalizar = document.querySelector("#errorLocalizar")
+const errorTerminos = document.querySelector("#errorTerminos")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,5 +37,14 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// fermer la modale
+function closeModal() {
+  modalbg.style.display = "none";
+}
+cerrar.addEventListener("click", closeModal);
 
+
+
+
+//validation prénom
 
